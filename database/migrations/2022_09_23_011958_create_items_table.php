20b55e85186a4b268d32b634a972d6e963dcf3a1
DESKTOP-4VMJ9PI\西條一樹 <riana.masato5005@gmail.com>
2022-09-26 20:54:39 +0900

    カラムの追加

--- database/migrations/2022_09_23_011958_create_items_table.php
+++ database/migrations/2022_09_23_011958_create_items_table.php
@@ -14,7 +14,15 @@ class CreateItemsTable extends Migration
     public function up()
     {
         Schema::create('items', function (Blueprint $table) {
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+
             $table->id();
+            $table->bigInteger('user_id');
+            $table->string('name',100);
+            $table->string('status',100);
+            $table->tinyInteger('type');
+            $table->string('detail',500);
             $table->timestamps();
         });
     }
