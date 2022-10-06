3dde3483bf6ef60c9678fa5e50d33cf5bb5dece4
marina <beth.eliz.vic@gmail.com>
2022-10-06 20:56:20 +0900

    20221006_ms

---
+++ resources/views/parts/navi.blade.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark mb-3">
+        <a class="navbar-brand" href="#">商品管理システム</a>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link active" href="#">ユーザー管理</span></a>
+                </li>
+                <!-- <li class="nav-item">
+                    <a class="nav-link" href="#">コンテンツ</a>
+                </li> -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        商品一覧
+                    </a>
+                    <!-- <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">検索</a> -->
+                        <!-- <a class="dropdown-item" href="#"></a> -->
+                    <!-- </div> -->
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        ログアウト
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">あいうえお</a>
+                      
+                    </div>
+                </li>
+            </ul>
+            </div>
+    </nav>
